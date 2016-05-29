42e0d2ef9a2ae6f4ca31991210d153d29c62c24f
Juan Andres Garcia C <JAGC@imac-de-juan.local>
2016-05-29 15:41:43 -0500

    Initial Commit

---
+++ Miniretoo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Miniretoo
+//
+//  Created by Juan Andres Garcia C on 29/05/16.
+//  Copyright (c) 2016 Juan Andres Garcia C. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
